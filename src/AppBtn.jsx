a3b2cccc98ec9ec2d8e6b1181c42d7cd86966145
Dalvan Siebra <dalvansiebra@outlook.com>
2023-10-21 12:14:27 -0300

    add btn

---
+++ src/AppBtn.jsx
@@ -0,0 +1,12 @@
+function AppBtn () {
+    return (
+        // não pode retornar mais de um elemento soltos/separados
+        // colocando dentro de um fragmento, conseguimos colocar mais de um elemento dentro do mesmo fragmento <></>
+
+        <>
+        <button>Botão TESTE</button>
+        </>
+    )
+}
+
+export default AppBtn
